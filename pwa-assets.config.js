1c91abb2a09f80f407753f211a1803756b08d1a5
wuchengfan <1004143951@qq.com>
2025-06-15 00:22:08 +0800

    refactor(pwa-assets): 更新PWA资源生成配置，添加透明图标尺寸
    扩展透明图标的尺寸配置以支持更多分辨率

--- pwa-assets.config.js
+++ pwa-assets.config.js
@@ -3,10 +3,21 @@ import {
     minimal2023Preset as preset,
 } from '@vite-pwa/assets-generator/config'
 
+console.log(preset)
+
 export default defineConfig({
+    transparent: {
+        sizes: [64, 72, 96, 128, 144, 152, 192, 384, 512],
+    },
     headLinkOptions: {
         preset: '2023',
     },
-    preset,
+    preset: {
+        ...preset,
+        transparent: {
+            ...preset.transparent,
+            sizes: [64, 72, 96, 128, 144, 152, 192, 384, 512],
+        }
+    },
     images: ['public/favicon.svg'],
 })

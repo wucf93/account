6700369c0bda6671d2fb4033fa4717d0a16c66c2
wuchengfan <1004143951@qq.com>
2025-06-14 23:10:23 +0800

    feat: 初始化项目基础结构和配置
    添加React应用基础文件结构，包括主入口文件、组件、样式和PWA支持
    配置Vite构建工具、ESLint代码规范及.gitignore文件

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: 'account-pwa',
+        short_name: 'account-pwa',
+        description: '记账',
+        theme_color: '#ffffff',
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: true,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    })],
+})

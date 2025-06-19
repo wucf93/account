8f20f72e4f81180f45b546b24df9e0449fe3f373
wuchengfan <1004143951@qq.com>
2025-06-19 23:25:55 +0800

    refactor: 将项目重构为pnpm workspace结构，移动pwa相关代码到packages/pwa目录

--- packages/pwa/vite.config.ts
+++ packages/pwa/vite.config.ts
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // @ts-ignore
     tailwindcss(),
     react(),
     VitePWA({
